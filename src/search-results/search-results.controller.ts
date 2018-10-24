24a44f7f7fc5552ad1c02ec45e69f42c1e7f892a
Ross Treadway <ross@alantreadway.net>
2018-10-24 13:59:24 +0100

    Squashed commit of the following:
    commit a5d07c6f062e70ed3e2abb15f91a63e63e998e09
    Merge: f3ab1e2 ccca331
    Author: Ross Treadway <ross@alantreadway.net>
    Date:   Wed Oct 24 13:58:33 2018 +0100
    
        Merge branch 'wip-geocoding' of github.com:Driftydirt/FindMyBusAPI into wip-geocoding
    
    commit ccca331af5ac67323bea68e597af3595aa3bdbf8
    Author: Alan Treadway <alan@alantreadway.net>
    Date:   Wed Oct 24 13:55:53 2018 +0100
    
        Switch to environment based Google API key
    
    commit f3ab1e212e0d7475b5fe35585c45523ec3f1eef6
    Merge: 40aa5a6 ed5073f
    Author: Ross Treadway <ross@alantreadway.net>
    Date:   Wed Oct 24 13:57:19 2018 +0100
    
        Merge branch 'wip-geocoding' of github.com:Driftydirt/FindMyBusAPI into wip-geocoding
    
    commit ed5073f8c69816daab8d34de14a29dcca0b64e29
    Author: Alan Treadway <alan@alantreadway.net>
    Date:   Wed Oct 24 13:55:53 2018 +0100
    
        Switch to environment based Google API key
    
    commit 40aa5a601138bdb64676e3b8f78b141a091ea330
    Author: Alan Treadway <alan@alantreadway.net>
    Date:   Wed Oct 24 13:55:53 2018 +0100
    
        Switch to environment based Google API key
    
    commit ae92e9a93a4b6c4a14064c30df7390aabf4e88d4
    Author: Ross Treadway <ross@alantreadway.net>
    Date:   Wed Oct 24 13:39:20 2018 +0100
    
        wip

--- src/search-results/search-results.controller.ts
+++ src/search-results/search-results.controller.ts
@@ -1,10 +1,34 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Logger } from '@nestjs/common';
 import { SearchResults } from './search-results.interface';
+import * as maps from '@google/maps';
 
 @Controller('search-results')
 export class SearchResultsController {
+  private readonly googleMapsClient: maps.GoogleMapsClient;
+
+  public constructor() {
+    this.googleMapsClient = maps.createClient({
+      key: process.env.GOOGLE_API_KEY,
+      timeout: 10_000,
+      Promise,
+    });
+  }
+
   @Get()
-  search(@Param('fromLocation') fromLocation, @Param('toLocation') toLocation) {
+  async search(
+    @Param('fromLocation') fromLocation,
+    @Param('toLocation') toLocation,
+  ): Promise<SearchResults> {
+    const location = '1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA';
+
+    Logger.log({ fromLocation, toLocation }, 'Perfoming geo-location lookup');
+
+    const response = await this.googleMapsClient
+      .geocode({ address: location })
+      .asPromise();
+    console.log({ results: JSON.stringify(response.json) });
+    const locationCoords = response.json.results[0].geometry.location;
+
     const mockResults: SearchResults = {
       fromLocation,
       toLocation,
