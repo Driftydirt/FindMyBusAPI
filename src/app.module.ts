030345c581256841dcbb7a548f46b4d637036e12
Ross Treadway <ross@alantreadway.net>
2018-11-06 22:07:22 +0000

    removing redundant files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { SearchResultsController } from './search-results/search-results.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import 'reflect-metadata';
+
+import { SearchResultsController } from 'controllers/search-results.controller';
+import { BusRouteService } from 'services/bus-route.service';
+import { BusStopService } from 'services/bus-stop.service';
+import { BusStop } from 'entities/bus-stop.entity';
+import { BusRoute } from 'entities/bus-route.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController, SearchResultsController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'BorderLands 2',
+      database: 'findmybus',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([BusStop, BusRoute]),
+  ],
+  controllers: [SearchResultsController],
+  providers: [BusRouteService, BusStopService],
 })
 export class AppModule {}
